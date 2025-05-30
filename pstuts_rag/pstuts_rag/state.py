12e5a8c4e5afcebbe6880b29e279580260ec9c8c
Marko Budisic <mbudisic@gmail.com>
2025-05-30 14:58:05 -0400

    feat: Introduce StateInput and StateOutput dataclasses for structured input and output handling

--- pstuts_rag/pstuts_rag/state.py
+++ pstuts_rag/pstuts_rag/state.py
@@ -1,11 +1,24 @@
+from dataclasses import dataclass, field
 from langchain_core.messages import BaseMessage
 
 
 import operator
-from typing import Annotated, List, TypedDict
+from typing import Annotated, List, Tuple, TypedDict
 
 
 class PsTutsTeamState(TypedDict):
     messages: Annotated[List[BaseMessage], operator.add]
     team_members: List[str]
     next: str
+
+
+@dataclass(kw_only=True)
+class StateInput:
+    input_query: str = field(default=None)  # Report topic
+
+
+@dataclass(kw_only=True)
+class StateOutput:
+    output_content: str = field(default=None)  # Final report
+    video_references: List[Tuple[str, float]] = field(default=None)
+    url_references: List[str] = field(default=None)
